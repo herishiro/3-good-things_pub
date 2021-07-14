3a5bfe1b2869f3d70531a6c09a05545adbdfba80
Yoshida-aisamurai <46587135+Yoshida-aisamurai@users.noreply.github.com>
2021-07-14 14:50:50 +0900

    cleanup repo

--- tests/globalTypes.ts
+++
@@ -1,9 +0,0 @@
-import { Browser, Page, ElementHandle, BrowserContext } from "playwright";
-
-declare global {
-  const page: Page;
-  const browser: Browser;
-  const browserName: string;
-  const elementHandle: ElementHandle
-  const context: BrowserContext
-}
